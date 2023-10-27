e6f4313a0f88585ce895517fbc867bf7e4ad9f43
Boni Garcia <boni.garcia@uc3m.es>
2023-10-27 15:08:26 +0200

    Include proxy tests

---
+++ src/test/java/io/github/bonigarcia/selenium/proxy/ChromeProxyTest.java
@@ -0,0 +1,57 @@
+/*
+ * (C) Copyright 2023 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.selenium.proxy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+class ChromeProxyTest {
+
+    WebDriver driver;
+
+    @BeforeEach
+    void setup() {
+        Proxy proxy = new Proxy();
+        proxy.setHttpProxy("localhost:8080");
+        ChromeOptions options = new ChromeOptions();
+        options.setProxy(proxy);
+
+        driver = new ChromeDriver(options);
+    }
+
+    @Disabled("Proxy not available in CI")
+    @Test
+    void test() {
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+        String title = driver.getTitle();
+        assertThat(title).contains("Selenium WebDriver");
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+}
