6ec928733f68ab7e854a465ab2776044a5aa7a22
Boni Garcia <boni.garcia@uc3m.es>
2023-10-30 11:41:37 +0100

    Include Selenium tests using remote browsers listenting in port 4444

---
+++ src/test/java/io/github/bonigarcia/selenium/remote/RemoteChromeTest.java
@@ -0,0 +1,53 @@
+/*
+ * (C) Copyright 2023 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.selenium.remote;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URL;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+class RemoteChromeTest {
+
+    WebDriver driver;
+
+    @BeforeEach
+    void setupTest() throws Exception {
+        ChromeOptions options = new ChromeOptions();
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/"),
+                options);
+    }
+
+    @Test
+    void test() {
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+        String title = driver.getTitle();
+        assertThat(title).contains("Selenium WebDriver");
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+}
