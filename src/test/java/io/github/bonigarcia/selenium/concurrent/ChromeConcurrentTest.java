26fdac08a16f45c34b921279ee85f7151edeb34f
Boni Garcia <boni.garcia@uc3m.es>
2023-11-06 15:09:28 +0100

    Include test to run several sessions in parallel

---
+++ src/test/java/io/github/bonigarcia/selenium/concurrent/ChromeConcurrentTest.java
@@ -0,0 +1,60 @@
+/*
+ * (C) Copyright 2023 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.selenium.concurrent;
+
+import static java.util.concurrent.Executors.newFixedThreadPool;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+class ChromeConcurrentTest {
+
+    static final int NUM_BROWSERS = 5;
+
+    @Test
+    void test() throws InterruptedException {
+        ExecutorService executorService = newFixedThreadPool(NUM_BROWSERS);
+        CountDownLatch latch = new CountDownLatch(NUM_BROWSERS);
+
+        for (int i = 0; i < NUM_BROWSERS; i++) {
+            executorService.submit(() -> {
+                try {
+                    checkPage();
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await(60, SECONDS);
+        executorService.shutdown();
+    }
+
+    void checkPage() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+        assertThat(driver.getTitle()).contains("Selenium WebDriver");
+        driver.close();
+    }
+
+}
