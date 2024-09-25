c31b1d65987d15756fb6074b7ccf1cafb5cd1894
Boni Garcia <boni.garcia@uc3m.es>
2024-09-25 19:43:50 +0200

    Include sample bidi action tests

---
+++ src/test/java/io/github/bonigarcia/selenium/bidi/action/FirefoxBidiActionTest.java
@@ -0,0 +1,67 @@
+/*
+ * (C) Copyright 2024 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.selenium.bidi.action;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.bidi.module.Input;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.interactions.Actions;
+
+class FirefoxBidiActionTest {
+
+    WebDriver driver;
+
+    @BeforeEach
+    void setup() {
+        FirefoxOptions options = new FirefoxOptions();
+        options.enableBiDi();
+        driver = new FirefoxDriver(options);
+    }
+
+    @Test
+    void test() {
+        driver.get(
+                "https://bonigarcia.dev/selenium-webdriver-java/web-form.html");
+
+        WebElement inputText = driver.findElement(By.name("my-text"));
+
+        String textValue = "Hello World!";
+        Input input = new Input(driver);
+        Actions actions = new Actions(driver);
+        Actions sendKeys = actions.sendKeys(inputText, textValue);
+        input.perform(driver.getWindowHandle(), sendKeys.getSequences());
+
+        assertThat(inputText.getAttribute("value")).isEqualTo(textValue);
+
+        inputText.clear();
+        assertThat(inputText.getAttribute("value")).isEmpty();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+}
