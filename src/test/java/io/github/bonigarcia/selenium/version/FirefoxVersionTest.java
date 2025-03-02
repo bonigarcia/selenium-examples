de5cd6759489980fd2522d9d3a8ae012e781d36d
Boni Garcia <boni.garcia@uc3m.es>
2025-03-02 20:10:50 +0100

    Store firefox-version-test output as XML file

--- src/test/java/io/github/bonigarcia/selenium/version/FirefoxVersionTest.java
+++ src/test/java/io/github/bonigarcia/selenium/version/FirefoxVersionTest.java
@@ -19,6 +19,8 @@ package io.github.bonigarcia.selenium.version;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -35,11 +37,13 @@ class FirefoxVersionTest {
     WebDriver driver;
 
     @BeforeEach
-    void setup() {
+    void setup() throws Exception {
         Arrays.stream(Logger.getLogger("").getHandlers()).forEach(handler -> {
             handler.setLevel(Level.FINE);
         });
         Logger.getLogger(SeleniumManager.class.getName()).setLevel(Level.FINE);
+        Handler handler = new FileHandler("firefox-version-test.xml");
+        Logger.getLogger(SeleniumManager.class.getName()).addHandler(handler);
 
         // https://www.selenium.dev/documentation/webdriver/drivers/options/#browserversion
         FirefoxOptions options = new FirefoxOptions();
