fe1d816d3414b9c8de7bc13fa36c88b7be0da96b
Boni Garcia <boni.garcia@uc3m.es>
2025-03-25 21:25:20 +0100

    Revert "Remove temporally WebDriverManager tests"
    This reverts commit 705480bce40c459bf22297af73fdbffcfe1d0ce7.

---
+++ src/test/java/io/github/bonigarcia/selenium/wdm/docker/DockerChromeRecordingTest.java
@@ -0,0 +1,56 @@
+/*
+ * (C) Copyright 2024 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.selenium.wdm.docker;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+class DockerChromeRecordingTest {
+
+    WebDriver driver;
+    WebDriverManager wdm;
+
+    @BeforeEach
+    void setupTest() {
+        wdm = WebDriverManager.chromedriver().browserInDocker()
+                .enableRecording();
+        driver = wdm.create();
+    }
+
+    @Test
+    void test() {
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+        assertThat(driver.getTitle()).contains("Selenium WebDriver");
+    }
+
+    @AfterEach
+    void teardown() throws InterruptedException {
+        // FIXME: pause for manual browser inspection
+        Thread.sleep(Duration.ofSeconds(3).toMillis());
+
+        wdm.quit();
+    }
+
+}
