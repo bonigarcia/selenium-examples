6673cf6f183b54b84b46952aca1df8c905e29a39
Boni Garcia <boni.garcia@uc3m.es>
2025-08-18 18:16:13 +0200

    Include cross-browser tests based on JUnit's @ParameterizedClass

---
+++ src/test/java/io/github/bonigarcia/selenium/cross_browser/CrossBrowserValueSourceTest.java
@@ -0,0 +1,56 @@
+/*
+ * (C) Copyright 2025 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.selenium.cross_browser;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.Parameter;
+import org.junit.jupiter.params.ParameterizedClass;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+@ParameterizedClass
+@ValueSource(classes = { ChromeDriver.class, FirefoxDriver.class })
+class CrossBrowserValueSourceTest {
+
+    @Parameter
+    Class<WebDriver> driverClass;
+
+    WebDriver driver;
+
+    @BeforeEach
+    void setup() throws Exception {
+        driver = driverClass.getDeclaredConstructor().newInstance();
+    }
+
+    @Test
+    void test() {
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+        assertThat(driver.getTitle()).contains("Selenium WebDriver");
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+}
