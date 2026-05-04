146b24ac04f327d35f37185ddf1d54f69a22a890
gyan4701 <gyan.ranjan@jadeglobal.com>
2026-05-04 11:34:49 +0530

    tst

--- src/test/java/LeadLocators.java
+++ src/test/java/LeadLocators.java
@@ -3,7 +3,7 @@ package locators;
 import org.openqa.selenium.By;
 
 public class LeadLocators {
-    public static final By NEW_BUTTON = By.cssSelector("button[name='New']");
+    public static final By TEMP_NEW_BUTTON = By.cssSelector("button[name='New']");
     public static final By SAVE_EDIT_BUTTON = By.cssSelector("button[name='SaveEdit']");
     public static final By FIRST_NAME_INPUT = By.cssSelector("lightning-input-field[field-name='FirstName'] input");
     public static final By LAST_NAME_INPUT = By.cssSelector("lightning-input-field[field-name='LastName'] input");
