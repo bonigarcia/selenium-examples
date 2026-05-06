83d78c33d46ed244618ef03884be092b0b0414c8
gyan4701 <gyan.ranjan@jadeglobal.com>
2026-05-06 22:57:46 +0530

    Add Selenium Java automation script: TC004

--- selenium/java/locators/AccountLocators.java
+++ selenium/java/locators/AccountLocators.java
@@ -1,32 +1,10 @@
-// NEW COMMENT
 package locators;
-
+// NEW and TEST
 import org.openqa.selenium.By;
-public class AccountLocators {
-    // App Launcher and Navigation
-    public static final By APP_LAUNCHER_BUTTON = By.cssSelector("div.slds-icon-waffle"); // Reusing existing elem_xycr1r
-    public static final By APP_LAUNCHER_SEARCH_INPUT = By.cssSelector("input.slds-input[placeholder='Search apps and items...']"); // Reusing existing elem_iutqq6
-    public static final By ACCOUNTS_APP_LAUNCHER_ITEM = By.xpath("//mark[text()='Accounts']/ancestor::a"); // Reusing existing elem_b2qekh
-    public static final By GLOBAL_SEARCH_INPUT = By.cssSelector("input[placeholder='Search Salesforce']"); // Reusing existing elem_sdqp9b
 
-    // Account Detail Page Elements
-    public static final By ACCOUNT_DETAIL_PAGE_HEADER_TITLE = By.xpath("//h1[contains(@class, 'slds-page-header__title')]"); // Part of existing elem_4yem9e context
-    public static final By TCV_AMOUNT_DISPLAY_FIELD = By.cssSelector("lightning-output-field[field-name='TCV_Amount__c'] lightning-formatted-text");
-    public static final By EDIT_BUTTON_HIGHLIGHT_PANEL = By.xpath("//button[@name='Edit']"); // Reusing existing edit
-
-    // Account Edit Modal Elements
-    public static final By EDIT_MODAL_TITLE = By.cssSelector("div.slds-modal__header h2.slds-modal__title");
-    public static final By TCV_AMOUNT_INPUT_EDIT_MODAL = By.cssSelector("lightning-input-field[field-name='TCV_Amount__c'] input[type='text']"); // Reusing existing tCVAmountc and refining by type
-    public static final By SAVE_EDIT_BUTTON = By.cssSelector("button[name='SaveEdit']"); // Reusing existing saveEdit
-    public static final By CANCEL_EDIT_BUTTON = By.cssSelector("button[name='CancelEdit']");
-
-    // Error Messages
-    public static final By VALIDATION_ERROR_MESSAGE_IN_MODAL = By.cssSelector("div.forceVisualMessageList ul li");
-
-    private AccountLocators() {}
-
-    // Dynamic locators for search results
-    public static By getAccountSearchResultLink(String accountName) {
-        return By.xpath("//a[@title='" + accountName + "']"); // Reusing existing elem_q06wto
-    }
+class AccountLocators {
+    public static final By NAME_INPUT = By.cssSelector("lightning-input-field[field-name='Name'] input");
+    public static final By TYPE_COMBOBOX = By.cssSelector("lightning-combobox[field-name='Type']");
+    public static final By OWNER_LOOKUP = By.cssSelector("lightning-lookup[field-name='OwnerId'] input");
+    // Add more locators as needed
 }
